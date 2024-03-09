8264a530ef71eadc7579e74250e48995c936e124
zhuyonghao <3113267308@qq.com>
2024-03-09 11:50:26 +0800

    replace, math, search, sort, swap, copy_backward, merge, unique and reverse in the stl algorithm

--- STL/stlAlgorithm.cpp
+++ STL/stlAlgorithm.cpp
@@ -105,13 +105,15 @@ int main()
     cout << c1 << (result ? "is less than" : "is greater than or equal to ")
          << c2 << endl;
 }
-*/
+
 
 // remove、remove_if、remove_copy、remove_copy_if
-// remove删除所有x元素
-// remove_copy删除所有x元素将其复制到另一个容器中
-// remove_if删除所有满足函数的元素
-// remove_if删除所有满足函数的元素将其复制到另一个容器中
+
+ //remove和remove_if
+ //并不修改这个容器中的元素数目，而是把那些没有被删除的元素移动到容器的前部
+ //remove_copy和remove_copy_if
+ //那些没有被删除的元素将其复制到另一个容器中
+
 #include <algorithm>
 #include <iostream>
 #include <array>
@@ -123,49 +125,352 @@ bool greater9(int);
 
 int main()
 {
-    const size_t SIZE = 10;
-    array<int, SIZE> init = {10, 2, 10, 4, 16, 6, 14, 8, 12, 10};
-    ostream_iterator<int> output(cout, " ");
+     const size_t SIZE = 10;
+     array<int, SIZE> init = {10, 2, 10, 4, 16, 6, 14, 8, 12, 10};
+     ostream_iterator<int> output(cout, " ");
 
-    array<int, SIZE> a1(init);
-    cout << "a1 before removing all 10s\n";
-    copy(a1.cbegin(), a1.cend(), output);
+     array<int, SIZE> a1(init);
+     cout << "a1 before removing all 10s\n";
+     copy(a1.cbegin(), a1.cend(), output);
 
-    auto newLastElement = remove(a1.begin(), a1.end(), 10);
-    cout << "\na1 after removing all 10s:\n";
-    copy(a1.begin(), newLastElement, output);
+     auto newLastElement = remove(a1.begin(), a1.end(), 10);
+     cout << "\na1 after removing all 10s:\n";
+     copy(a1.begin(), newLastElement, output);
 
-    array<int, SIZE> a2(init);
-    array<int, SIZE> c = {0};
-    cout << "\na2 before removing all 10s and copying:\n";
-    copy(a2.cbegin(), a2.cend(), output);
+     array<int, SIZE> a2(init);
+     array<int, SIZE> c = {0};
+     cout << "\na2 before removing all 10s and copying:\n";
+     copy(a2.cbegin(), a2.cend(), output);
 
-    remove_copy(a2.cbegin(), a2.cend(), c.begin(), 10);
-    cout << "\nc after removing all 10s from a2:\n";
-    copy(c.cbegin(), c.cend(), output);
+     remove_copy(a2.cbegin(), a2.cend(), c.begin(), 10);
+     cout << "\nc after removing all 10s from a2:\n";
+     copy(c.cbegin(), c.cend(), output);
 
-    array<int, SIZE> a3(init);
-    cout << "\na3 before removing all elements greater than 9:\n   ";
-    copy(a3.begin(), a3.end(), output);
+     array<int, SIZE> a3(init);
+     cout << "\na3 before removing all elements greater than 9:\n   ";
+     copy(a3.begin(), a3.end(), output);
+     // predicate"指的是返回布尔值（true或false）的函数或“函数对象”
+     // greater9是一个函数对象用来断言
+     newLastElement = remove_if(a3.begin(), a3.end(), greater9);
+     cout << "\na3 after removing all elements greater than 9:\n";
+     copy(a3.begin(), newLastElement, output);
 
-    newLastElement = remove_if(a3.begin(), a3.end(), greater9);
-    cout << "\na3 after removing all elements greater than 9:\n";
-    copy(a3.begin(), newLastElement, output);
+     array<int, SIZE> a4(init);
+     array<int, SIZE> c2 = {0};
+     cout << "\n\na4 before all elements"
+          << "\ngreater than 9 and copying:\n";
+     copy(a4.cbegin(), a4.cend(), output);
 
-    array<int, SIZE> a4(init);
-    array<int, SIZE> c2 = {0};
-    cout << "\n\na4 before all elements"
-         << "\ngreater than 9 and copying:\n";
-    copy(a4.cbegin(), a4.cend(), output);
-
-    remove_copy_if(a4.cbegin(), a4.cend(), c2.begin(), greater9);
-    cout << "\nc2 after removing all elements"
-         << "\ngreater than 9 from a4:\n";
-    copy(c2.cbegin(), c2.cend(), output);
-    cout << endl;
+     remove_copy_if(a4.cbegin(), a4.cend(), c2.begin(), greater9);
+     cout << "\nc2 after removing all elements"
+          << "\ngreater than 9 from a4:\n";
+     copy(c2.cbegin(), c2.cend(), output);
+     cout << endl;
 }
 
 bool greater9(int x)
 {
-    return x > 9;
+     return x > 9;
+}
+
+
+// replace、replace_if、replace_copy和replace_copy_if
+#include <algorithm>
+#include <iostream>
+#include <array>
+#include <iterator>
+
+using namespace std;
+
+bool greater9(int);
+
+int main()
+{
+     const size_t SIZE = 10;
+     array<int, SIZE> init = {10, 2, 10, 4, 16, 6, 14, 8, 12, 10};
+     ostream_iterator<int> output(cout, " ");
+
+     array<int, SIZE> a1(init);
+     cout << "a1 before replacing all 10s:\n";
+     copy(a1.cbegin(), a1.cend(), output);
+
+     replace(a1.begin(), a1.end(), 10, 100);
+     cout << "\na1 after replacing all 10s:\n";
+     copy(a1.cbegin(), a1.cend(), output);
+
+     array<int, SIZE> a2(init);
+     array<int, SIZE> c1;
+     cout << "\n\na2 before replacing all 10s and copying:\n";
+     copy(a2.cbegin(), a2.cend(), output);
+
+     replace_copy(a2.cbegin(), a2.cend(), c1.begin(), 10, 100);
+     cout << "\na2 after replacing all 10s in c1:\n";
+     copy(c1.cbegin(), c1.cend(), output);
+
+     array<int, SIZE> a3(init);
+     cout << "\n\na3 before replacing values greater9:\n";
+     copy(a3.cbegin(), a3.cend(), output);
+
+     replace_if(a3.begin(), a3.end(), greater9, 100);
+     cout << "\na3 after replacing values greater9:\n";
+     copy(a3.cbegin(), a3.cend(), output);
+
+     array<int, SIZE> a4(init);
+     array<int, SIZE> c2;
+     cout << "\n\na4 before replacing values greater9 and copying:\n";
+     copy(a4.cbegin(), a4.cend(), output);
+
+     replace_copy_if(a4.cbegin(), a4.cend(), c2.begin(), greater9, 100);
+     cout << "\na4 after replacing values greater9 and coping:\n";
+     copy(c2.cbegin(), c2.cend(), output);
+}
+
+bool greater9(int x)
+{
+     return x > 9;
+}
+
+
+// math algorithm
+// random_shuffle(洗牌) count count_if min_element(返回迭代器) max_element minmax_element
+// accumulate(求和) for_each transform
+#include <algorithm>
+#include <iostream>
+#include <numeric>
+#include <array>
+#include <iterator>
+
+using namespace std;
+
+bool greater9(int);
+void outputSquare(int);
+int calculateCube(int);
+
+int main()
+{
+     const size_t SIZE = 10;
+     array<int, SIZE> a1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+     ostream_iterator<int> output(cout, " ");
+
+     cout << "a1 before random_shuffle: ";
+     copy(a1.cbegin(), a1.cend(), output);
+
+     random_shuffle(a1.begin(), a1.end());
+     cout << "\na1 after random_shuffle: ";
+     copy(a1.cbegin(), a1.cend(), output);
+
+     array<int, SIZE> a2 = {100, 2, 8, 1, 50, 3, 8, 8, 9, 10};
+     cout << "\n\na2 contains: ";
+     copy(a2.cbegin(), a2.cend(), output);
+
+     int result = count(a2.cbegin(), a2.cend(), 8);
+     cout << "\nNumber of elements matching 8: " << result;
+
+     result = count_if(a2.cbegin(), a2.cend(), greater9);
+     cout << "\nNumber of elements graeter 9: " << result;
+
+     cout << "\n\nMinimum element in a2 is: "
+          << *(min_element(a2.cbegin(), a2.cend()));
+
+     auto minAndMax = minmax_element(a2.cbegin(), a2.cend());
+     cout << "\nThe minimum and maximum elements in a2 are "
+          << *minAndMax.first << " and " << *minAndMax.second
+          << ", repectively";
+
+     cout << "\n\nThe total of the elements in a1 is: "
+          << accumulate(a1.cbegin(), a1.cend(), 0);
+
+     cout << "\n\nThe square of every integer in a1 is:\n";
+     for_each(a1.cbegin(), a1.cend(), outputSquare);
+     array<int, SIZE> cubes;
+
+     transform(a1.cbegin(), a1.cend(), cubes.begin(), calculateCube);
+     cout << "\n\nThe cube of every integer in a1 is:\n";
+     copy(cubes.cbegin(), cubes.cend(), output);
+     cout << endl;
+}
+
+bool greater9(int value)
+{
+     return value > 9;
+}
+
+void outputSquare(int value)
+{
+     cout << value * value << ' ';
+}
+
+int calculateCube(int value)
+{
+     return value * value * value;
+}
+
+
+// 基本查找和排序算法
+// find find_if  sort binary_search
+// all_of any_of none_of find_if_not
+
+#include <algorithm>
+#include <iostream>
+#include <array>
+#include <iterator>
+using namespace std;
+
+bool greater10(int);
+
+int main()
+{
+     const size_t SIZE = 10;
+     array<int, SIZE> a = {10, 2, 17, 5, 16, 8, 13, 11, 20, 7};
+     ostream_iterator<int> output(cout, " ");
+
+     cout << "array contains: ";
+     copy(a.cbegin(), a.cend(), output);
+
+     auto location = find(a.cbegin(), a.cend(), 16);
+
+     if (location != a.cend())
+          cout << "\n\nFound 16 at location " << (location - a.cbegin());
+     else
+          cout << "\n\n16 not found";
+
+     location = find(a.cbegin(), a.cend(), 100);
+
+     if (location != a.cend())
+          cout << "\n\nFound 100 at location " << (location - a.cbegin());
+     else
+          cout << "\n\n100 not found";
+
+     location = find_if(a.cbegin(), a.cend(), greater10);
+
+     if (location != a.cend())
+          cout << "\n\nThe first value greater than 10 is " << *location
+               << "\n found at location " << (location - a.cbegin());
+     else
+          cout << "\n\nvalues greater10 not found";
+
+     sort(a.begin(), a.end());
+     cout << "\n\narray after sort: ";
+     copy(a.cbegin(), a.cend(), output);
+
+     if (binary_search(a.cbegin(), a.cend(), 13))
+          cout << "\n\n13 was found in a";
+     else
+          cout << "\n\n13 was not found in a";
+
+     if (binary_search(a.cbegin(), a.cend(), 100))
+          cout << "\n\n100 was found in a";
+     else
+          cout << "\n\n100 was not found in a";
+     // 检测是不是全部大于
+     if (all_of(a.cbegin(), a.cend(), greater10))
+          cout << "\n\nall the elements in a are greater than 10";
+     else
+          cout << "\n\nSome elements in a are not greater than 10";
+     // 检测是不是有一些大于
+     if (any_of(a.cbegin(), a.cend(), greater10))
+          cout << "\n\nSome of the elements in a greater than 10";
+     else
+          cout << "\n\nNone of elements in a are greater than 10";
+     // 检测是不是没有
+     if (none_of(a.cbegin(), a.cend(), greater10))
+          cout << "\n\nNone of the elements in a greater than 10";
+     else
+          cout << "\n\nsome of elements in a are greater than 10";
+
+     location = find_if_not(a.cbegin(), a.cend(), greater10);
+
+     if (location != a.cend())
+          cout << "\n\nThe first value not greater than 10 is " << *location
+               << "\nfound at location " << (location - a.cbegin());
+     else
+          cout << "\n only value greater than 10 were found";
+
+     cout << endl;
+}
+
+bool greater10(int value)
+{
+     return value > 10;
+}
+
+
+// swap iter_swap swap_ranges
+#include <iostream>
+#include <array>
+#include <algorithm>
+#include <iterator>
+
+using namespace std;
+
+int main()
+{
+     const size_t SIZE = 10;
+     array<int, SIZE> a = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+     ostream_iterator<int> output(cout, " ");
+
+     cout << "Array contains: \n";
+     copy(a.cbegin(), a.cend(), output);
+
+     swap(a[0], a[1]);
+
+     cout << "\nAfter swapping, Array contains: \n";
+     copy(a.cbegin(), a.cend(), output);
+     // 交换迭代器值没变
+     iter_swap(a.begin(), a.begin() + 1);
+     cout << "\nAfter swapping iterator, Array contains: \n";
+     copy(a.cbegin(), a.cend(), output);
+     // 用从第五位开始的值赋值给前五位
+     swap_ranges(a.begin(), a.begin() + 5, a.begin() + 5);
+
+     cout << "\nAfter swapping range, Array contains: \n";
+     copy(a.cbegin(), a.cend(), output);
+     cout << endl;
+}
+*/
+
+// copy_backward merge unique reverse
+#include <iostream>
+#include <algorithm>
+#include <array>
+#include <iterator>
+
+using namespace std;
+
+int main()
+{
+     const size_t SIZE = 5;
+     array<int, SIZE> a1 = {1, 2, 3, 4, 5};
+     array<int, SIZE> a2 = {6, 7, 8, 9, 10};
+     ostream_iterator<int> output(cout, " ");
+
+     cout << "a1 contains: \n";
+     copy(a1.cbegin(), a1.cend(), output);
+     cout << "\na2 contains: \n";
+     copy(a2.cbegin(), a2.cend(), output);
+
+     array<int, SIZE> result;
+     // 将容器a1中的元素逆序复制到result容器中
+     // result容器从后往前返回一个迭代器
+     copy_backward(a1.cbegin(), a1.cend(), result.end());
+     cout << "\n\nAfter copy_backward, results contains: ";
+     copy(result.cbegin(), result.cend(), output);
+
+     array<int, SIZE + SIZE> result2;
+
+     merge(a1.cbegin(), a1.cend(), a2.cbegin(), a2.cend(), result2.begin());
+
+     cout << "\n\nAfter merge: ";
+     copy(result2.cbegin(), result2.cend(), output);
+     // 上将每个重复的元素移至范围的尾部
+     auto endLocation = unique(result2.begin(), result2.end());
+
+     cout << "\n\nAfter unique contains: ";
+     copy(result2.begin(), endLocation, output);
+
+     cout << "\nAfter reverse: ";
+     reverse(a1.begin(), a1.end());
+     copy(a1.cbegin(), a1.cend(), output);
+
+     cout << endl;
 }

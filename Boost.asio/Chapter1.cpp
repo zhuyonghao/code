319a6e22f56ed7a514abf0182454fae4b1db1c64
zhuyonghao <3113267308@qq.com>
2024-03-31 19:28:06 +0800

    Chapter1 completed

--- Boost.asio/Chapter1.cpp
+++ Boost.asio/Chapter1.cpp
@@ -9,7 +9,7 @@ using namespace boost;
 
 int main()
 {
-	// Step 1. assume that the client application has 
+	// Step 1. assume that the client application has
 	// obtained the IP-address and the protocal port number
 	std::string raw_ip_address = "192.168.1.0";
 	unsigned short port_num = 3333;
@@ -27,7 +27,7 @@ int main()
 
 	if (ec.value() != 0)
 	{
-		// Provided IP address is invalid. Breaking execution.  
+		// Provided IP address is invalid. Breaking execution.
 		std::cout
 			<< "Failed to parse the IP address. Error code = "
 			<< ec.value() << ". Message: " << ec.message();
@@ -68,7 +68,7 @@ int main()
 	// Step 3.
 	asio::ip::tcp::endpoint ep(ip_address, port_num);
 
-	// Step 4. The endpoint is created and can be used to 
+	// Step 4. The endpoint is created and can be used to
 	// specifies the IP addresses and a port number on which
 	// the server application wants to listen for incoming
 	// connections.
@@ -151,7 +151,7 @@ int main()
 	}
 
 	return 0;
-}*/
+}
 
 
 // Create a passive socket
@@ -188,6 +188,361 @@ int main()
 			<< "Failed to open the acceptor socket! Error code = "
 			<< ec.value() << ". Message: " << ec.message();
 		return ec.value();
+	}
+
+	return 0;
+}
+
+
+#include <iostream>
+#include <boost/asio.hpp>
+
+using namespace boost;
+
+int main()
+{
+	// Step 1. Assume that the client application has already
+	// obtained the DNS name and protocol port number and
+	// represented them as strings.
+	std::string host = "baidu.com";
+	std::string port_num = "3333";
+
+	// Step 2.
+	asio::io_service ios;
+
+	// Step 3. Creating a query
+	asio::ip::tcp::resolver::query resolver_query(host,
+		port_num, asio::ip::tcp::resolver::query::numeric_service);
+
+	// Step 4. Creating a resolver
+	asio::ip::tcp::resolver resolver(ios);
+
+	// Used to store information about error that happens
+	//  during the resolution process
+
+	boost::system::error_code ec;
+
+	// Step 5.
+	asio::ip::tcp::resolver::iterator it =
+		resolver.resolve(resolver_query, ec);
+
+	// Handing error if any
+	if (ec.value() != 0)
+	{
+		//Failed to resolve DNS name. breaking execution.
+		std::cout << "Failed to resolve the DNS name."
+			<< "Error code = " << ec.value()
+			<< ". Message = " << ec.message();
+		return ec.value();
+	}
+	return 0;
+}
+
+
+// resolve the DNS name   UDP
+#include <iostream>
+#include <boost/asio.hpp>
+
+using namespace boost;
+
+int main()
+{
+	// Step 1. Assume that the client application has already
+	// obtained the DNS name and protocol port number and
+	// represented them as strings.
+	std::string host = "baidu.com";
+	std::string port_num = "3333";
+
+	// Step 2.
+	asio::io_service ios;
+
+	// Step 3. Creating a query
+	asio::ip::udp::resolver::query resolver_query(host,
+		port_num, asio::ip::udp::resolver::query::numeric_service);
+
+	// Step 4. Creating a resolver.
+	asio::ip::udp::resolver resolver(ios);
+
+	// Used to store information about error that happens
+	// during the resolution process.
+	boost::system::error_code ec;
+
+	// Step 5.
+	asio::ip::udp::resolver::iterator it =
+		resolver.resolve(resolver_query, ec);
+
+	if (ec.value() != 0)
+	{
+		// Failed to resolve the DNS name. Breaking execution.
+		std::cout << "Failed to reslove a DNS name."
+			<< "Error code = " << ec.value()
+			<< ".Message = " << ec.message();
+
+		return ec.value();
+	}
+
+	asio::ip::udp::resolver::iterator it_end;
+
+	for (; it != it_end; ++it)
+		// Here we can access the endpoint like this.
+		asio::ip::udp::endpoint ep = it->endpoint();
+
+	return 0;
+}
+
+
+// Binding a socket to an endpoint
+#include <iostream>
+#include <boost/asio.hpp>
+
+using namespace boost;
+
+int main()
+{
+	// Step 1. Here we assume that the server application has
+	// already obtained the protocol port number.
+	unsigned short port_num = 3333;
+
+	// Step 2. Creating an endpoint
+	asio::ip::tcp::endpoint ep(asio::ip::address_v4::any(),
+		port_num);
+
+	// Used by acceptor class constructor
+	asio::io_service ios;
+
+	// Step 3. Creating and opening an acceptor socket.
+	asio::ip::tcp::acceptor acceptor(ios, ep.protocol());
+
+	boost::system::error_code ec;
+
+	// Step 4. Bing the acceptor socket.
+	acceptor.bind(ep, ec);
+
+	// Handing errors if any.
+	if (ec.value() != 0)
+	{
+		// Failed to bind the acceptor socket. Breaking
+		// execution.
+		std::cout << "Failed to bind the acceptor socket."
+			<< "Error code = " << ec.value() << ". Message: "
+			<< ec.message();
+
+		return ec.value();
+	}
+
+	return 0;
+}
+
+
+
+// UDP bind a socket
+#include <iostream>
+#include <boost/asio.hpp>
+
+using namespace boost;
+
+int main()
+{
+	// Step 1. Here we assume that the server application has
+	// already obtained the protocol number.
+	unsigned short port_num = 3333;
+
+	// Step 2. Creating an endpoint.
+	asio::ip::udp::endpoint ep(asio::ip::address_v4::any(),
+		port_num);
+
+	// Used by socket class constructor.
+	asio::io_service ios;
+
+	// Step 3. Creating and opening a socket.
+	asio::ip::udp::socket sock(ios, ep.protocol());
+
+	boost::system::error_code ec;
+
+	// Step 4. Binding the socket to an endpoint.
+	sock.bind(ep, ec);
+
+	// Handing errors if any.
+	if (ec.value() != 0)
+	{
+		// Failed to bind the acceptor socket. Breaking
+		// execution.
+		std::cout << "Failed to bind the acceptor socket."
+			<< "Error code = " << ec.value() << ". Message: "
+			<< ec.message();
+
+		return ec.value();
+	}
+
+	return 0;
+}
+
+
+#include <iostream>
+#include <boost/asio.hpp>
+
+using namespace boost;
+
+int main()
+{
+	// Step 1. Assume that the server application has already
+	// obtained the IP address and protocol port number of the
+	// targer server
+	std::string raw_ip_address = "192.168.0.1";
+
+	unsigned short port_num = 3333;
+
+	try
+	{
+		// Step 2. Creating an endpoint designating
+		// a target server application
+		asio::ip::tcp::endpoint
+			ep(asio::ip::address::from_string(raw_ip_address),
+				port_num);
+
+		asio::io_service ios;
+
+		// Step 3. Creating and opening a socket.
+		asio::ip::tcp::socket sock(ios, ep.protocol());
+
+		// Step 4. Connecting a socket.
+		sock.connect(ep);
+
+		// At this point socket 'sock' is connected to
+		// the server application and can be used
+		// to send data to or receive data from it
+	}
+	// Overload of asio::op::address::from_string() and
+	// asio::ip::tcp::socket::connect() used here throw
+	// exception in case of error condition
+
+	catch (system::system_error &e)
+	{
+		std::cout << "Error occured! Error code = " << e.code()
+			<< ". Message: " << e.what();
+
+		return e.code().value();
+	}
+	return 0;
+}
+
+
+// the socket's connect() method will bind the socket to the endpoint consisting
+//of an IP address and a protocol port number chosen by the operating system
+
+
+
+// DNS code
+#include <iostream>
+#include <boost/asio.hpp>
+
+using namespace boost;
+
+int main()
+{
+	// Step 1. Assume that the client application has already
+	// obtained the DNS name and protocol port number and
+	// represent them as strings
+	std::string host = "baidu.com";
+	std::string port_num = "3333";
+
+	// Used by a resolver and a socket.
+	asio::io_service ios;
+
+	// Creating a resolver's query
+	asio::ip::tcp::resolver::query resolver_query(host, port_num,
+		asio::ip::tcp::resolver::query::numeric_service);
+
+	// Creaing a resolver.
+	asio::ip::tcp::resolver resolver(ios);
+
+	try
+	{
+		// Step 2. Resolving a DNS name.
+		asio::ip::tcp::resolver::iterator it =
+			resolver.resolve(resolver_query);
+
+		// Step 3. Creating a socket.
+		asio::ip::tcp::socket sock(ios);
+
+		// Step 4. asio::connect() method iterator over
+		// each endpoint until successfully connects to one
+		// of them. It will throw an exception if it fails
+		// to connect to all the endpoints or if other
+		// error occurs.
+		asio::connect(sock, it);
+
+		// At this point socket 'sock' is connected to
+		// the server application and can be used to
+		// send data to or receive data from it.
+	}
+	// Overload of asio::ip::tcp::resolver::resolve and
+	// asio::connect() used here throw
+	// exceptions in case of error condition.
+	catch (system::system_error &e)
+	{
+		std::cout << "Error occured! Error code = " << e.code()
+			<< ". Message: " << e.what();
+
+		return e.code().value();
+	}
+	return 0;
+}
+*/
+
+// Accepting connections
+#include <iostream>
+#include <boost/asio.hpp>
+
+using namespace boost;
+
+int main()
+{
+	// The size of the queue containing the pending connection
+	// requests.
+	const int BACKLOG_SIZE = 30;
+
+	// Step 1. Here we assume that the server application has
+	// already obtained the protocol port number.
+	unsigned short port_num = 3333;
+
+	// Step 2. Creating a server endpoint.
+	asio::ip::tcp::endpoint ep(asio::ip::address_v4::any(),
+							   port_num);
+
+	asio::io_service ios;
+
+	try
+	{
+		// Step 3. Instantiating and opening an acceptor socket.
+		asio::ip::tcp::acceptor acceptor(ios, ep.protocol());
+
+		// Step 4. Binding the acceptor socket to the
+		// server endpoint.
+		acceptor.bind(ep);
+
+		// Step 5. Starting to listen for incoming connection
+		// requests.
+		acceptor.listen(BACKLOG_SIZE);
+
+		// Step 6. Creating an active socket.
+		asio::ip::tcp::socket sock(ios);
+
+		// Step 7. Processing the next connection request and
+		// connecting the active socket to the client.
+		acceptor.accept(sock);
+
+		// At this point 'sock' socket is connected to
+		// the client application and can be used to send data to
+		// or recive data from it.
+	}
+
+	catch (system::system_error &e)
+	{
+		std::cout << "Error occured! Error code = " << e.code()
+				  << ". Message: " << e.what();
+
+		return e.code().value();
 	}
 
 	return 0;

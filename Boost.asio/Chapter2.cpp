7640269c0535a781459088831a0c40ac05348563
zhuyonghao <3113267308@qq.com>
2024-04-02 11:31:59 +0800

    synchronous completed

--- Boost.asio/Chapter2.cpp
+++ Boost.asio/Chapter2.cpp
@@ -1,19 +1,19 @@
 /*
 // Using fixed length I/O buffer
-// Prepare a buffer for an output operation 
+// Prepare a buffer for an output operation
 #include <iostream>
 #include <boost/asio.hpp>
 
 using namespace boost;
 
-int main() 
+int main()
 {
     const std::string buf = "Hello"; // 'buf' is raw buffer.
-    
+
     // Step 3. Creating buffer representation that satisfies
     // ConstBufferSequency concept requirements.
     asio::const_buffers_1 output_buf = asio::buffer(buf);
-    
+
     // Step 4. 'output_buf' is the representation of the
     // buffer 'buf' that can be used in Boost.Asio output
     // operations.
@@ -36,7 +36,7 @@ int main()
 
     // Step 1. Allocating the buffer.
     std::unique_ptr<char[]> buf(new char[BUF_SIZE_BYTES]);
-    
+
     // Step 2. Creating buffer representation that satisfies
     // MutableBufferSequence concept requirements.
     asio::mutable_buffers_1 input_buf =
@@ -78,7 +78,7 @@ int main()
 
     return 0;
 }
-*/
+
 
 #include <iostream>
 #include <boost/asio.hpp>
@@ -130,4 +130,54 @@ int main()
         return e.code().value();
     }
     return 0;
+}
+*/
+
+// Reading from a tcp socket synchronously
+#include <iostream>
+#include <boost/asio.hpp>
+
+using namespace boost;
+
+std::string readFromSocket(asio::ip::tcp::socket &sock)
+{
+    const unsigned char MESSAGE_SIZE = 7;
+    char buf[MESSAGE_SIZE];
+    std::size_t total_bytes_read = 0;
+
+    while (total_bytes_read != MESSAGE_SIZE)
+    {
+        total_bytes_read += sock.read_some(
+            asio::buffer(buf + total_bytes_read,
+                         MESSAGE_SIZE - total_bytes_read));
+    }
+
+    return std::string(buf, total_bytes_read);
+}
+
+int main()
+{
+    std::string raw_ip_address = "127.0.0.1";
+    unsigned short port_num = 3333;
+
+    try
+    {
+        asio::ip::tcp::endpoint
+            ep(asio::ip::address::from_string(raw_ip_address),
+               port_num);
+
+        asio::io_service ios;
+
+        asio::ip::tcp::socket sock(ios, ep.protocol());
+
+        sock.connect(ep);
+
+        readFromSocket(sock);
+    }
+    catch (system::system_error &e)
+    {
+        std::cout << "Error occured! Error code = " << e.code()
+                  << ". Message: " << e.what();
+        return e.code().value();
+    }
 }

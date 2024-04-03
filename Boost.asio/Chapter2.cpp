5c21289b2989d722f5d26fb0058787f8898c8aa5
zhuyonghao <3113267308@qq.com>
2024-04-03 21:36:12 +0800

    asynchronous writing operation

--- Boost.asio/Chapter2.cpp
+++ Boost.asio/Chapter2.cpp
@@ -131,7 +131,7 @@ int main()
     }
     return 0;
 }
-*/
+
 
 // Reading from a tcp socket synchronously
 #include <iostream>
@@ -139,7 +139,7 @@ int main()
 
 using namespace boost;
 
-std::string readFromSocket(asio::ip::tcp::socket &sock)
+std::string readFromSocket(asio::ip::tcp::socket& sock)
 {
     const unsigned char MESSAGE_SIZE = 7;
     char buf[MESSAGE_SIZE];
@@ -149,7 +149,8 @@ std::string readFromSocket(asio::ip::tcp::socket &sock)
     {
         total_bytes_read += sock.read_some(
             asio::buffer(buf + total_bytes_read,
-                         MESSAGE_SIZE - total_bytes_read));
+                MESSAGE_SIZE - total_bytes_read)
+        );
     }
 
     return std::string(buf, total_bytes_read);
@@ -164,7 +165,7 @@ int main()
     {
         asio::ip::tcp::endpoint
             ep(asio::ip::address::from_string(raw_ip_address),
-               port_num);
+                port_num);
 
         asio::io_service ios;
 
@@ -174,10 +175,133 @@ int main()
 
         readFromSocket(sock);
     }
-    catch (system::system_error &e)
+    catch (system::system_error& e)
     {
         std::cout << "Error occured! Error code = " << e.code()
-                  << ". Message: " << e.what();
+            << ". Message: " << e.what();
         return e.code().value();
     }
 }
+
+
+#include <iostream>
+#include <boost/asio.hpp>
+
+using namespace boost;
+
+// keeps objects we need in a callback to
+// identity whether all data has been written
+// to the socket and to initiate nect asnyc
+// writing operation if needed
+
+struct Session
+{
+    std::shared_ptr<asio::ip::tcp::socket> sock;
+    std::string buf;
+    std::size_t total_bytes_written;
+};
+
+// Function used as a callback for
+// asnychronous writing operation
+// Checks if all data from the buffer has
+// been written to the socket and initiates
+// new asynchronous writing operation if needed.
+void callback(const boost::system::error_code& ec,
+    std::size_t bytes_transferred,
+    std::shared_ptr<Session> s)
+{
+    if (ec.value() != 0)
+    {
+        std::cout << "Error occured! Error code = "
+            << ec.value()
+            << ". Message: " << ec.message();
+
+        return;
+    }
+
+    s->total_bytes_written += bytes_transferred;
+
+    if (s->total_bytes_written == s->buf.length())
+    {
+        return;
+    }
+
+    s->sock->async_write_some(
+        asio::buffer(
+            s->buf.c_str() +
+            s->total_bytes_written,
+            s->buf.length() -
+            s->total_bytes_written),
+        std::bind(callback, std::placeholders::_1,
+            std::placeholders::_2, s));
+}
+
+void writeToSocket(std::shared_ptr<asio::ip::tcp::socket> sock)
+{
+    std::shared_ptr<Session> s(new Session);
+
+    // Step 4. Allocating and filling the buffer.
+    s->buf = std::string("Hello");
+    s->total_bytes_written = 0;
+    s->sock = sock;
+
+    // Step 5. Initiating asynchronous write operation.
+    s->sock->async_write_some(
+        asio::buffer(s->buf),
+        std::bind(callback,
+            std::placeholders::_1,
+            std::placeholders::_2,
+            s)
+    );
+}
+
+int main()
+{
+    std::string raw_ip_address = "127.0.0.1";
+    unsigned short port_num = 3333;
+
+    try
+    {
+        asio::ip::tcp::endpoint
+            ep(asio::ip::address::from_string(raw_ip_address),
+                port_num);
+
+        asio::io_service ios;
+
+        // Step 3. Allocating, opening and connecting a socket.
+        std::shared_ptr<asio::ip::tcp::socket> sock(
+            new asio::ip::tcp::socket(ios, ep.protocol()));
+
+        sock->connect(ep);
+
+        writeToSocket(sock);
+
+        // Step 6.
+        ios.run();
+    }
+
+    catch (system::system_error& e)
+    {
+        std::cout << "Error occured! Error code = " << e.code()
+            << ". Message: " << e.what();
+
+        return e.code().value();
+    }
+
+    return 0;
+}
+*/
+
+#include <iostream>
+#include <boost/asio.hpp>
+
+using namespace boost;
+
+// Keeps objects we need in a callback to
+// identity whether all data has been read
+// from the socket and to initiate next async
+// reading operation if needed
+
+struct Session
+{
+};

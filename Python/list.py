664f516665f94616e3a3e318e944705e74d9f0b8
zhuyonghao <3113267308@qq.com>
2024-07-17 11:14:23 +0800

    list

---
+++ Python/list.py
@@ -0,0 +1,11 @@
+names = ["zhang", "zhao", "zhou", "zhu"]
+print(names)
+# print(names[-1].title())
+
+# message = f"My name is {names[-1].title()}."
+# print(message)
+# names.append("cai")
+# print(names)
+
+# names.insert(1, "xin")
+# print(names)

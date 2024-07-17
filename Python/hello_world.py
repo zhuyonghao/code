89d39e6541e5f7546b34d6c02cc7a8382ce6b8ec
zhuyonghao <3113267308@qq.com>
2024-07-17 11:13:11 +0800

    第二章

---
+++ Python/hello_world.py
@@ -0,0 +1,35 @@
+# print("hello world")
+
+# message = "Hello world !"
+# nest1 = "hello 'world'"
+# nest2 = '"hello world"'
+# print(message)
+
+# # title()显示每个单词的首字母大写
+# print(message.title())
+# # upper()显示大写
+# print(message.upper())
+# # lower()显示小写
+# print(message.lower())
+# first_name = "oliver"
+# last_name = "allen"
+# full_name = f"{first_name} {last_name}"
+# message = f"Hello {full_name.title()}!"
+# print(message)
+
+# message = " Hello world "
+# print(message.lstrip())
+# print(message.rstrip())
+# print(message.strip())
+
+# message = "I love you!"
+# print(message.removeprefix("I love "))
+
+# 1 billion
+# intger = 1_000_000_000
+# print(2 ** 3)
+# print(intger)
+# x, y, z = 1, 2, 3
+# print(x, y, z)
+
+import this

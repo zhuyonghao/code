077df5f4af18bc48e52513669f21e4bb6e1df741
zhuyonghao <3113267308@qq.com>
2024-07-17 18:19:25 +0800

    学到了if语句

--- Python/list.py
+++ Python/list.py
@@ -1,5 +1,5 @@
-names = ["zhang", "zhao", "zhou", "zhu"]
-print(names)
+# names = ["zhang", "zhao", "zhou", "zhu"]
+# print(names)
 # print(names[-1].title())
 
 # message = f"My name is {names[-1].title()}."
@@ -9,3 +9,86 @@ print(names)
 
 # names.insert(1, "xin")
 # print(names)
+
+# del names[-1]
+# print(names)
+
+# poped_name = names.pop()
+# print(poped_name)
+# print(names)
+
+# poped_name = names.pop(2)
+# print(poped_name)
+# print(names)
+
+# name = "zhu"
+# names.remove(name)
+# print(names)
+# print(f"This is {name.title()} !")
+
+# integers = [5, 2, 4, 9, 10, 1]
+# integers.sort(reverse = True)
+# print(integers)
+# print(sorted(integers,reverse = True))
+# print(integers)
+# integers.reverse()
+# print(len(integers))
+
+# for integer in integers:
+#     print(f"This is {integer} !")
+
+# for i in range(1, 6):
+#     print(i)
+
+# squared_numbers = []
+# for value in range(1, 11):
+#     squared_numbers.append(value ** 2)
+
+# print(squared_numbers)
+
+# numbers = list(range(1, 10, 2))
+# print(numbers)
+
+# digits = [1, 2, 3, 4, 5]
+# print(min(digits), max(digits), sum(digits))
+
+# square = [value ** 2 for value in range(1, 11)]
+# print(square)
+
+# numbers = [value ** 3 for value in range(1, 6)]
+# print(numbers)
+# print(numbers[1:5])
+# print(numbers[:3])
+# print(numbers[3:])
+# print(numbers[-3:])
+
+# for value in numbers[:3]:
+#     print(value)
+
+# integers1 = [1, 2, 3, 4, 5]
+# integers2 = integers1[:]
+# print(integers1)
+# print(integers2)
+# integers1.append(6)
+# integers2.append(7)
+# print(integers1)
+# print(integers2)
+
+# my_tuple = (1, 2, 3, 4, 5)
+# print(my_tuple)
+# print(my_tuple[1:])
+
+# numbers = (1, 2, 3, 4, 5)
+# for value in numbers:
+#     print(value)
+
+# my_tuple = (1, 2, 3)
+# print(my_tuple)
+# my_tuple = (1, 2, 3, 4, 5)
+# print(my_tuple)
+
+numbers = (1, 2, 3, 4, 5)
+if 10 not in numbers:
+    print("Yes")
+else:
+    print("not")
